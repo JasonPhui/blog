8b532a2267fb634d24c73ae25548c14424d56960
panhui <1586376147@qq.com>
2018-06-08 10:56:21 +0800

    合并到主分支

--- src/main/java/cn/ph/blog/core/configurer/MybatisConfigurer.java
+++ src/main/java/cn/ph/blog/core/configurer/MybatisConfigurer.java
@@ -29,7 +29,7 @@ public class MybatisConfigurer {
     public SqlSessionFactory sqlSessionFactoryBean(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
         factory.setDataSource(dataSource);
-        factory.setTypeAliasesPackage("cn.ph.blog.mode");
+        factory.setTypeAliasesPackage("cn.ph.blog.model");
         // 添加xml目录
         ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         factory.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
