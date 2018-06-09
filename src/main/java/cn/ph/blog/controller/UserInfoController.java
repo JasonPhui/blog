e06b40504302ab00082740e806f07c315be170b9
panhui <1586376147@qq.com>
2018-06-09 15:04:14 +0800

    添加异常测试方法

--- src/main/java/cn/ph/blog/controller/UserInfoController.java
+++ src/main/java/cn/ph/blog/controller/UserInfoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("userInfo")
@@ -21,6 +22,14 @@ public class UserInfoController {
     public RetResult<UserInfo> selectById(Integer id){
         UserInfo userInfo = userInfoService.selectById(id);
         System.out.println(userInfo);
+        return RetResponse.makeOKRsp(userInfo);
+    }
+
+    @PostMapping("/testException")
+    public RetResult<UserInfo> testException(Integer id){
+        List a = null;
+        a.size();
+        UserInfo userInfo = userInfoService.selectById(id);
         return RetResponse.makeOKRsp(userInfo);
     }
 }
