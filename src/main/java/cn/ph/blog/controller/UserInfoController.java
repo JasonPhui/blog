f4837091e538b03ded4d249ce824b84d835c3c43
panhui <1586376147@qq.com>
2018-06-10 16:45:58 +0800

    增加Swagger的注解

--- src/main/java/cn/ph/blog/controller/UserInfoController.java
+++ src/main/java/cn/ph/blog/controller/UserInfoController.java
@@ -4,8 +4,13 @@ import cn.ph.blog.core.ret.RetResponse;
 import cn.ph.blog.core.ret.RetResult;
 import cn.ph.blog.model.UserInfo;
 import cn.ph.blog.sevice.UserInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -13,15 +18,19 @@ import java.util.List;
 
 @RestController
 @RequestMapping("userInfo")
+@Api(tags = {"用户操作接口"}, description = "userInfoController")
 public class UserInfoController {
 
     @Resource
     private UserInfoService userInfoService;
 
+    @ApiOperation(value = "查询用户", notes = "根据用户ID查询用户")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer", paramType = "query")
+    })
     @PostMapping("/selectById")
-    public RetResult<UserInfo> selectById(Integer id){
+    public RetResult<UserInfo> selectById(@RequestParam  Integer id){
         UserInfo userInfo = userInfoService.selectById(id);
-        System.out.println(userInfo);
         return RetResponse.makeOKRsp(userInfo);
     }
 
