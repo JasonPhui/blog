35a609aa37e4b119a0da9cc5133f0921bf08b0d2
panhui <1586376147@qq.com>
2018-06-10 14:58:13 +0800

    添加SwaggerConfigurer配置类

---
+++ src/main/java/cn/ph/blog/core/configurer/SwaggerConfigurer.java
@@ -0,0 +1,40 @@
+package cn.ph.blog.core.configurer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * SwaggerConfigurer配置类
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfigurer {
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("cn.ph.blog.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("mySpringBoot 使用Swagger2构建RESTful APIs")
+                .description("************")
+                .termsOfServiceUrl("@@@@@@@@@@@@@@@@@@")
+                .contact(new Contact("WoodCharcoal", "https://github.com/WoodCharcoal", null))
+                .version("1.0")
+                .build();
+    }
+}
