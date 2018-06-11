92d6b4e6cac9a8c0426c0dd6dce19396896fca49
panhui <1586376147@qq.com>
2018-06-11 20:55:10 +0800

    添加带分页的查询接口

--- src/main/java/cn/ph/blog/sevice/UserInfoService.java
+++ src/main/java/cn/ph/blog/sevice/UserInfoService.java
@@ -1,7 +1,9 @@
 package cn.ph.blog.sevice;
 
 import cn.ph.blog.model.UserInfo;
+import com.github.pagehelper.PageInfo;
 
 public interface UserInfoService {
     UserInfo selectById(Integer id);
+    PageInfo<UserInfo> selectAll(Integer page, Integer size);
 }
