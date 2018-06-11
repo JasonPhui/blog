db20d94e5af431ce37e32325a4634621034ae53d
panhui <1586376147@qq.com>
2018-06-11 19:45:06 +0800

    添加查询所有用户方法

--- src/main/java/cn/ph/blog/dao/UserInfoMapper.java
+++ src/main/java/cn/ph/blog/dao/UserInfoMapper.java
@@ -3,6 +3,9 @@ package cn.ph.blog.dao;
 import cn.ph.blog.model.UserInfo;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface UserInfoMapper {
     UserInfo selectById(@Param("id") Integer id);
+    List<UserInfo> selectALL();
 }
