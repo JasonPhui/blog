b3540a3bb3dfe0d9244be12fe457e36ebd060847
panhui <1586376147@qq.com>
2018-06-14 12:02:54 +0800

    通过freemarker生成的service

---
+++ src/main/java/cn/ph/blog/service/SystemLogService.java
@@ -0,0 +1,11 @@
+package cn.ph.blog.service;
+
+import cn.ph.blog.model.SystemLog;
+import cn.ph.blog.core.universal.Service;
+
+/**
+* @Description: SystemLogService接口
+*/
+public interface SystemLogService extends Service<SystemLog> {
+
+}
---
+++ src/main/java/cn/ph/blog/service/impl/SystemLogServiceImpl.java
@@ -0,0 +1,22 @@
+package cn.ph.blog.service.impl;
+
+import cn.ph.blog.dao.SystemLogMapper;
+import cn.ph.blog.model.SystemLog;
+import cn.ph.blog.service.SystemLogService;
+import cn.ph.blog.core.universal.AbstractService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+/**
+* @Description: SystemLogService接口实现类
+*/
+@Service
+public class SystemLogServiceImpl extends AbstractService<SystemLog> implements SystemLogService {
+
+    @Resource
+    private SystemLogMapper systemLogMapper;
+
+}
+
