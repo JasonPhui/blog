2854552fb1d883b22ca1f0446c646cd17b1ad379
panhui <1586376147@qq.com>
2018-06-14 12:00:30 +0800

    添加可通过freemarker生成service与controller的代码

--- src/test/java/cn/ph/blog/CodeGenerator.java
+++ src/test/java/cn/ph/blog/CodeGenerator.java
@@ -3,12 +3,18 @@ package cn.ph.blog;
 import cn.ph.blog.core.constant.ProjectConstant;
 import com.alibaba.druid.util.StringUtils;
 import com.google.common.base.CaseFormat;
+import freemarker.template.TemplateExceptionHandler;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.*;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 代码生成器，根据数据表名称生成对应的Model、Mapper简化开发。
@@ -21,6 +27,16 @@ public class CodeGenerator {
     private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
     private static final String JAVA_PATH = "src/main/java"; // java文件路径
     private static final String RESOURCES_PATH = "src/main/resources";// 资源文件路径
+
+    // 模板位置
+    private static final String TEMPLATE_FILE_PATH = "src/test/java/resources/template/generator";
+    // 生成的Service存放路径
+    private static final String PACKAGE_PATH_SERVICE = packageConvertPath(ProjectConstant.SERVICE_PACKAGE);
+    // 生成的Service实现存放路径
+    private static final String PACKAGE_PATH_SERVICE_IMPL = packageConvertPath(ProjectConstant.SERVICE_IMPL_PACKAGE);
+    // 生成的Controller存放路径
+    private static final String PACKAGE_PATH_CONTROLLER = packageConvertPath(ProjectConstant.CONTROLLER_PACKAGE);
+
 
     /**
      * genCode("输入表名","输入自定义Model名称");
@@ -55,6 +71,8 @@ public class CodeGenerator {
      */
     public static void genCode(String tableName, String modelName) {
         genModelAndMapper(tableName, modelName);
+        genService(tableName);
+        genController(tableName);
     }
 
     public static void genModelAndMapper(String tableName, String modelName) {
@@ -105,6 +123,64 @@ public class CodeGenerator {
         System.out.println(modelName + ".java 生成成功");
         System.out.println(modelName + "Mapper.java 生成成功");
         System.out.println(modelName + "Mapper.xml 生成成功");
+    }
+
+    public static void genService(String tableName) {
+        try {
+            freemarker.template.Configuration cfg = getConfiguration();
+            //模板所需要的参数
+            Map<String, Object> data = new HashMap<>();
+            String modelNameUpperCamel = tableNameConvertUpperCamel(tableName);
+            data.put("modelNameUpperCamel", modelNameUpperCamel);
+            data.put("modelNameLowerCamel", CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, modelNameUpperCamel));
+            data.put("basePackage", ProjectConstant.BASE_PACKAGE);
+            data.put("basePackageService", ProjectConstant.SERVICE_PACKAGE);
+            data.put("basePackageServiceImpl", ProjectConstant.SERVICE_IMPL_PACKAGE);
+            data.put("basePackageModel", ProjectConstant.MODEL_PACKAGE);
+            data.put("basePackageDao", ProjectConstant.MAPPER_PACKAGE);
+
+            File file = new File(JAVA_PATH + PACKAGE_PATH_SERVICE + modelNameUpperCamel + "Service.java");
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            cfg.getTemplate("service.ftl").process(data, new FileWriter(file));
+            System.out.println(modelNameUpperCamel + "Service.java 生成成功");
+
+            File file1 = new File(JAVA_PATH + PACKAGE_PATH_SERVICE_IMPL + modelNameUpperCamel + "ServiceImpl.java");
+            if (!file1.getParentFile().exists()) {
+                file1.getParentFile().mkdirs();
+            }
+            cfg.getTemplate("service-impl.ftl").process(data, new FileWriter(file1));
+            System.out.println(modelNameUpperCamel + "ServiceImpl.java 生成成功");
+        } catch (Exception e) {
+            throw new RuntimeException("生成Service失败", e);
+        }
+    }
+
+    public static void genController(String tableName) {
+        try {
+            freemarker.template.Configuration cfg = getConfiguration();
+            Map<String, Object> data = new HashMap<>();
+            String modelNameUpperCamel = tableNameConvertUpperCamel(tableName);
+            data.put("baseRequestMapping", CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, modelNameUpperCamel));
+            data.put("modelNameUpperCamel", modelNameUpperCamel);
+            data.put("modelNameLowerCamel", CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, modelNameUpperCamel));
+            data.put("basePackage", ProjectConstant.BASE_PACKAGE);
+            data.put("basePackageController", ProjectConstant.CONTROLLER_PACKAGE);
+            data.put("basePackageService", ProjectConstant.SERVICE_PACKAGE);
+            data.put("basePackageModel", ProjectConstant.MODEL_PACKAGE);
+
+            File file = new File(JAVA_PATH + PACKAGE_PATH_CONTROLLER + modelNameUpperCamel + "Controller.java");
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            cfg.getTemplate("controller.ftl").process(data, new FileWriter(file));
+
+            System.out.println(modelNameUpperCamel + "Controller.java 生成成功");
+        } catch (Exception e) {
+            throw new RuntimeException("生成Controller失败", e);
+        }
+
     }
 
     private static Context getContext(){
@@ -169,4 +245,18 @@ public class CodeGenerator {
     private static String tableNameConvertUpperCamel(String tableName) {
         return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, tableName.toLowerCase());
     }
+
+    private static freemarker.template.Configuration getConfiguration() throws IOException {
+        freemarker.template.Configuration cfg = new freemarker.template.Configuration(freemarker.template.Configuration.VERSION_2_3_23);
+        cfg.setDirectoryForTemplateLoading(new File(TEMPLATE_FILE_PATH));
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.IGNORE_HANDLER);
+        return cfg;
+    }
+
+
+    private static String packageConvertPath(String packageName) {
+        return String.format("/%s/", packageName.contains(".") ? packageName.replaceAll("\\.", "/") : packageName);
+    }
+
 }
