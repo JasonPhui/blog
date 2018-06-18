ece1d6c6ae6c5ebd4b1eabe303affcc08097eda8
panhui <1586376147@qq.com>
2018-06-18 23:05:35 +0800

    通过用户id获取权限

--- src/main/java/cn/ph/blog/dao/db1/RolePermMapper.java
+++ src/main/java/cn/ph/blog/dao/db1/RolePermMapper.java
@@ -3,5 +3,8 @@ package cn.ph.blog.dao.db1;
 import cn.ph.blog.core.universal.Mapper;
 import cn.ph.blog.model.RolePerm;
 
+import java.util.List;
+
 public interface RolePermMapper extends Mapper<RolePerm> {
+    List<String> getPermsByUserId(String userId);
 }
