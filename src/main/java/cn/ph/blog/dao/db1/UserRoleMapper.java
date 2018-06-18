6343f57dfc1867f13cec90103cecdeabe2bf0ab1
panhui <1586376147@qq.com>
2018-06-18 23:06:55 +0800

    通过用户ID获取所有角色

--- src/main/java/cn/ph/blog/dao/db1/UserRoleMapper.java
+++ src/main/java/cn/ph/blog/dao/db1/UserRoleMapper.java
@@ -3,5 +3,8 @@ package cn.ph.blog.dao.db1;
 import cn.ph.blog.core.universal.Mapper;
 import cn.ph.blog.model.UserRole;
 
+import java.util.List;
+
 public interface UserRoleMapper extends Mapper<UserRole> {
+    List<String> getRolesByUserId(String userId);
 }
