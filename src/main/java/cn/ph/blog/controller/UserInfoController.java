f13d3d73ecc09c34559081913a8eeb80670d8caa
panhui <1586376147@qq.com>
2018-06-18 23:02:08 +0800

    增加shiro认证登录方法

--- src/main/java/cn/ph/blog/controller/UserInfoController.java
+++ src/main/java/cn/ph/blog/controller/UserInfoController.java
@@ -2,6 +2,7 @@ package cn.ph.blog.controller;
 
 import cn.ph.blog.core.ret.RetResponse;
 import cn.ph.blog.core.ret.RetResult;
+import cn.ph.blog.core.ret.ServiceException;
 import cn.ph.blog.model.UserInfo;
 import cn.ph.blog.service.UserInfoService;
 import com.github.pagehelper.PageInfo;
@@ -9,6 +10,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,7 +35,7 @@ public class UserInfoController {
             @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer", paramType = "query")
     })
     @PostMapping("/selectById")
-    public RetResult<UserInfo> selectById(@RequestParam  String id){
+    public RetResult<UserInfo> selectById(@RequestParam String id) {
         UserInfo userInfo = userInfoService.selectById(id);
         return RetResponse.makeOKRsp(userInfo);
     }
@@ -42,16 +47,31 @@ public class UserInfoController {
     })
     @PostMapping("/selectAll")
     public RetResult<PageInfo<UserInfo>> selectAll(@RequestParam(defaultValue = "0") Integer page,
-                                                   @RequestParam(defaultValue = "0") Integer size){
+                                                   @RequestParam(defaultValue = "0") Integer size) {
         PageInfo<UserInfo> pageInfo = userInfoService.selectAllByPage(page, size);
         return RetResponse.makeOKRsp(pageInfo);
     }
 
     @PostMapping("/testException")
-    public RetResult<UserInfo> testException(String id){
+    public RetResult<UserInfo> testException(String id) {
         List a = null;
         a.size();
         UserInfo userInfo = userInfoService.selectById(id);
         return RetResponse.makeOKRsp(userInfo);
+    }
+
+    @PostMapping("/login")
+    public RetResult<UserInfo> login(String userName, String password) {
+        System.out.println(userName + password);
+        Subject currentUser = SecurityUtils.getSubject();
+        //登录
+        try {
+            currentUser.login(new UsernamePasswordToken(userName, password));
+        } catch (IncorrectCredentialsException i) {
+            throw new ServiceException("密码输入错误");
+        }
+        //从session取出用户信息
+        UserInfo user = (UserInfo) currentUser.getPrincipal();
+        return RetResponse.makeOKRsp(user);
     }
 }
