a0a3400f21c01a26dad0f445ce80be4c7aac46a3
panhui <1586376147@qq.com>
2018-06-19 22:45:07 +0800

    添加批量插入日志

--- src/main/java/cn/ph/blog/dao/db1/SystemLogMapper.java
+++ src/main/java/cn/ph/blog/dao/db1/SystemLogMapper.java
@@ -3,5 +3,8 @@ package cn.ph.blog.dao.db1;
 import cn.ph.blog.core.universal.Mapper;
 import cn.ph.blog.model.SystemLog;
 
+import java.util.List;
+
 public interface SystemLogMapper extends Mapper<SystemLog> {
+    Integer insertByBatch(List<SystemLog> list);
 }
