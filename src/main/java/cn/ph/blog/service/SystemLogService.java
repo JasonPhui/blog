5f5d136420ae7840b9a3baf609147bd72596c3cd
panhui <1586376147@qq.com>
2018-06-19 22:45:24 +0800

    批量插入日志

--- src/main/java/cn/ph/blog/service/SystemLogService.java
+++ src/main/java/cn/ph/blog/service/SystemLogService.java
@@ -3,9 +3,11 @@ package cn.ph.blog.service;
 import cn.ph.blog.model.SystemLog;
 import cn.ph.blog.core.universal.Service;
 
+import java.util.List;
+
 /**
 * @Description: SystemLogService接口
 */
 public interface SystemLogService extends Service<SystemLog> {
-
+    Integer insertByBatch(List<SystemLog> list);
 }
--- src/main/java/cn/ph/blog/service/impl/SystemLogServiceImpl.java
+++ src/main/java/cn/ph/blog/service/impl/SystemLogServiceImpl.java
@@ -7,6 +7,7 @@ import cn.ph.blog.core.universal.AbstractService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
 * @Description: SystemLogService接口实现类
@@ -17,5 +18,8 @@ public class SystemLogServiceImpl extends AbstractService<SystemLog> implements 
     @Resource
     private SystemLogMapper systemLogMapper;
 
+    public Integer insertByBatch(List<SystemLog> list){
+        return systemLogMapper.insertByBatch(list);
+    }
 }
 
