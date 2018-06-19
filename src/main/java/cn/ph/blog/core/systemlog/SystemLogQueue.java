1301a2b1192c0ff6450de551c0fcb51a1eb5596f
panhui <1586376147@qq.com>
2018-06-19 22:44:40 +0800

    日志队列

---
+++ src/main/java/cn/ph/blog/core/systemlog/SystemLogQueue.java
@@ -0,0 +1,23 @@
+package cn.ph.blog.core.systemlog;
+
+import cn.ph.blog.model.SystemLog;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class SystemLogQueue {
+
+    private BlockingQueue<SystemLog> blockingQueue = new LinkedBlockingQueue<>();
+
+    public void add(SystemLog systemLog) {
+        blockingQueue.add(systemLog);
+    }
+
+    public SystemLog poll() throws InterruptedException {
+        return blockingQueue.poll(1, TimeUnit.SECONDS);
+    }
+}
+
